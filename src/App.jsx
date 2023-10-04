47a9cfe256595a4e8b099065a4d6b168de45f7f0
Victor Vahid <123102911+VictorVahid@users.noreply.github.com>
2023-10-04 14:48:59 +0000

    Estilização do Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className="card">
         <h2>Hawkeye</h2>
-        <img src="https://pbs.twimg.com/media/FEp6sS-UYAE1MUj.jpg" />
+        <img src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/f/f5/Hawkeye_Char_Poster.jpg/revision/latest?cb=20211118181820" />
       </div>
     </>
   )
