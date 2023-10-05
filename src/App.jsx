68967e520d6009c5bb3502a17d2151c33fdbfd10
Victor Vahid <123102911+VictorVahid@users.noreply.github.com>
2023-10-05 14:12:00 +0000

    Carregamento dinâmico de items com 'map'

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import Card from './components/Card/Card'
 function App() {
   const personagem1 = {
     nome: 'Hawkeye',
-    imagemUrl:'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/f/f5/Hawkeye_Char_Poster.jpg/revision/latest?cb=20211118181820'
+    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/b/bb/Clint_Hawkeye.jpg/revision/latest?cb=20211229222209&path-prefix=pt'
   }
   const personagem2 = {
     nome: 'Kate Bishop',
-    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/3/31/Kate_Bishop_Character_Poster.jpg/revision/latest?cb=20220628042411&path-prefix=pt'
+    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/3/31/Kate_Bishop_Character_Poster.jpg/revision/latest/scale-to-width-down/1000?cb=20220628042411&path-prefix=pt'
   }
   const personagem3 = {
-    nome: 'Kate Bishop',
+    nome: 'Viúva Negra/Yelena Belova',
     imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/5/52/Yelena_Belova.jpg/revision/latest?cb=20220712135207&path-prefix=pt'
   }
+
+  //array (usa-se [])
+  const personagens = [personagem1,personagem2,personagem3]
+
   return (
     <>
-      <Card item= {personagem1}/>
-      <Card item= {personagem2}/>
-      <Card item= {personagem3}/>
+      {personagens.map(function(personagem) {
+        return <Card item={personagem} />
+      })}
     </>
   )
 }
@@ -26,4 +30,3 @@ function App() {
 export default App
 
 
-// O problema do layout é que os cards ficam indo um abaixo do outro 
