02d0a50752fe0ead2e1f1fac2a99fc3e21083dfb
Victor Vahid <123102911+VictorVahid@users.noreply.github.com>
2023-10-05 12:55:11 +0000

    Criação do componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Hawkeye</h2>
-        <img src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/f/f5/Hawkeye_Char_Poster.jpg/revision/latest?cb=20211118181820" />
-      </div>
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css'
+
+ export default function Card() {
+    return(
+        <div className="card">
+            <h2>Hawkeye</h2>
+            <img src="https://static.wikia.nocookie.net/marvelcinematicuniverse/images/f/f5/Hawkeye_Char_Poster.jpg/revision/latest?cb=20211118181820" />
+        </div>
+    )
+}
