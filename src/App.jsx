e36133ed9058539e0cfcd983dc2912770ae5c2e3
Victor Vahid <123102911+VictorVahid@users.noreply.github.com>
2023-10-05 16:03:19 +0000

    Desafio 2 (array+map)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css'
 import Card from './components/Card/Card'
 
+{/* <Tag text = "Status:Vivo" />
+<Tag text = "Espécie:Humana" />
+<Tag text = "Origem: Terra" /> */}
+
 function App() {
   const personagem1 = {
     nome: 'Hawkeye',
-    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/b/bb/Clint_Hawkeye.jpg/revision/latest?cb=20211229222209&path-prefix=pt'
+    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/b/bb/Clint_Hawkeye.jpg/revision/latest?cb=20211229222209&path-prefix=pt',
+    tags: ['Status:Vivo','Espécie:Humana','Origem: Terra']
   }
   const personagem2 = {
     nome: 'Kate Bishop',
-    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/3/31/Kate_Bishop_Character_Poster.jpg/revision/latest/scale-to-width-down/1000?cb=20220628042411&path-prefix=pt'
+    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/3/31/Kate_Bishop_Character_Poster.jpg/revision/latest/scale-to-width-down/1000?cb=20220628042411&path-prefix=pt',
+    tags:["oi TINHA"]
   }
   const personagem3 = {
     nome: 'Yelena Belova',
-    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/5/52/Yelena_Belova.jpg/revision/latest?cb=20220712135207&path-prefix=pt'
+    imagemUrl:'https://static.wikia.nocookie.net/universocinematograficomarvel/images/5/52/Yelena_Belova.jpg/revision/latest?cb=20220712135207&path-prefix=pt',
+    tags:["vadia sai da daqui sua puitaaaa"]
   }
 
   //array (usa-se [])
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,18 @@ import './Card.css'
 
  export default function Card(props) {
     const item = props.item;
+
+    const tags = item.tags ||[];
+
     return(
         <div className="card">
             <h2>{item.nome}</h2>
 
-        <div className="tag-list">
-            <Tag text = "Status:Vivo" />
-            <Tag text = "Espécie:Humana" />
-            <Tag text = "Origem: Terra" />
+        <div className="tags-list">
+            {tags.map(function(tag) {
+                return <Tag text={tag} />
+            })}
         </div>
-
-
-
 
             <img src={item.imagemUrl} />
         </div> 
